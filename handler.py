9d6ec66b786632b6f924d09ed01212aec674c6e9
Lucas Coppio <lucascoppio@gmail.com>
2021-04-27 21:02:46 -0300

    added function that acts as cloudwatch agent and sends data on lambda size back to cloudwatch

--- handler.py
+++ handler.py
@@ -1,7 +1,12 @@
 from argparse import Namespace
-from clear_lambda_storage import remove_old_lambda_versions
+from clear_lambda_storage import remove_old_lambda_versions, calculate_all_lambdas_size
 
 
 def clear_lambda_storage(event, context):
     remove_old_lambda_versions(Namespace(token_key_id=None, token_secret=None, regions=None, profile=None))
     return "Successful clean! 🗑 ✅"
+
+
+def calculate_total_lambda_size(event, context):
+    calculate_all_lambdas_size(Namespace(token_key_id=None, token_secret=None, regions=None, profile=None))
+    return "Successful count! 🗑 ✅"
